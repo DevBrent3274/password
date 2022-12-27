ed10ec27c170123309fd1e0b35c4b4731143c764
Brent Johnson <brentjohnson3274@gmail.com>
2022-12-27 15:10:09 -0600

    Arrays filled out and Ifs finished

--- homework/password/Develop/script.js
+++ homework/password/Develop/script.js
@@ -1,14 +1,15 @@
 // Assignment Code
+// Added global variables which include arrays for stoarage of generated characteres and all available characters to choose from
 var generateBtn = document.querySelector("#generate");
 var numberOfcharacters;
 var okToUseLowercase;
 var okToUseUppercase;
 var okToUseNumbers;
 var okToUseSpecialChars;
-var specialChars = ["!","@"];
-var numbers = ["1","2"];
-var lowerCase = ["a","b",];
-var upperCase = ["A","B"];
+var specialChars = ["!","@","#","$","%","^","&","*","(",")","-","+","=","'",",",".","/",":",";","<",">","?","[","]","_","{","}","|"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var passwordArray = [];
 
 
@@ -20,7 +21,7 @@ function askQuestions(){
   okToUseNumbers = confirm("Do you want to use numbers?");
   okToUseSpecialChars = confirm("Do you want to use special characters?");
   
-  // if number of characrters no tin range??
+  // if number of characrters not in range??  
 // if choices are this or that, use for loop to generate password based on selections
 }
 
@@ -33,10 +34,12 @@ function generatePassword(){
   if(okToUseUppercase == true){
     passwordArray = passwordArray.concat(upperCase);
   }
-  if(okToUseLowercase == true){
-    passwordArray = passwordArray.concat(lowerCase);
+  if(okToUseNumbers == true){
+    passwordArray = passwordArray.concat(numbers);
   }
-    
+  if(okToUseSpecialChars == true){
+    passwordArray = passwordArray.concat(specialChars);
+  }
 
   for (i=0; i<numberOfCharacters; i++) {
     let random = Math.floor(Math.random()*passwordArray.length)
